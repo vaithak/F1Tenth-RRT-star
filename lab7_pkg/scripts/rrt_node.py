1dd61c0a6591a5cd638924a4f2ae3fcf6ef31a99
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2025-03-12 19:29:18 -0400

    Good but slow working version of RRT*

--- lab7_pkg/scripts/rrt_node.py
+++ lab7_pkg/scripts/rrt_node.py
@@ -137,22 +137,28 @@ class RRT(Node):
         super().__init__('rrt_node')
         self.get_logger().info("RRT Node has been initialized")
         
-        self.declare_parameter('lookahead', 5.0)
-        self.declare_parameter('max_distance', 2.0)
-        self.declare_parameter('cell_size', 0.05)
-        self.declare_parameter('goal_bias', 0.05)
+        self.declare_parameter('lookahead', 4.0)
+        self.declare_parameter('max_steer_distance', 0.8)
+        self.declare_parameter('min_waypoint_tracking_distance', 0.8)
+        self.declare_parameter('waypoint_close_enough', 0.8)
+        self.declare_parameter('cell_size', 0.1)
+        self.declare_parameter('goal_bias', 0.1)
         self.declare_parameter('goal_close_enough', 0.05)
         self.declare_parameter('obstacle_inflation_radius', 0.20)
-        self.declare_parameter('num_rrt_points', 800)
+        self.declare_parameter('num_rrt_points', 100)
+        self.declare_parameter('neighborhood_radius', 1.0)
         self.declare_parameter('waypoint_file', '/home/vaithak/Downloads/UPenn/F1Tenth/sim_ws/src/sampling-based-motion-planning-team6/waypoints/fitted_waypoints.csv')
 
         self.lookahead = self.get_parameter('lookahead').value
-        self.max_distance = self.get_parameter('max_distance').value
+        self.max_steer_distance = self.get_parameter('max_steer_distance').value
+        self.min_waypoint_tracking_distance = self.get_parameter('min_waypoint_tracking_distance').value
+        self.waypoint_close_enough = self.get_parameter('waypoint_close_enough').value
         self.cell_size = self.get_parameter('cell_size').value
         self.goal_bias = self.get_parameter('goal_bias').value
         self.goal_close_enough = self.get_parameter('goal_close_enough').value
         self.obstacle_inflation_radius = self.get_parameter('obstacle_inflation_radius').value
         self.num_rrt_points = self.get_parameter('num_rrt_points').value
+        self.neighborhood_radius = self.get_parameter('neighborhood_radius').value
         waypoint_file = self.get_parameter('waypoint_file').value
         self.waypoints = np.genfromtxt(waypoint_file, delimiter=',')
 
@@ -178,7 +184,7 @@ class RRT(Node):
         self.grid_vis_pub_ = self.create_publisher(OccupancyGrid, '/rrt/grid', 10)
         self.path_vis_pub_ = self.create_publisher(Marker, '/rrt/path', 10)
         self.tree_vis_pub_ = self.create_publisher(MarkerArray, '/rrt/tree', 10)
-        self.waypoint_to_track_pub_ = self.create_publisher(PointStamped, '/rrt/waypoint_to_track', 10)
+        self.waypoint_to_track_pub_ = self.create_publisher(Marker, '/rrt/waypoint_to_track', 10)
 
         # Create an occupancy grid of nav2_msgs/OccupancyGrid type
         self.grid_bounds_x = (0.0, self.lookahead)
@@ -191,12 +197,14 @@ class RRT(Node):
 
         # Create a list to store the tree nodes
         self.tree = []
+        self.current_following_waypoint = None
+        self.grid_formed = False
 
         # Variables for pure pursuit
         self.prev_curvature = None
         self.kp_gain = 0.4
         self.kd_gain = 0.0
-        self.adaptive_speed = lambda curvature: 0.8
+        self.adaptive_speed = lambda curvature: 1.5
 
 
     def scan_callback(self, scan_msg):
@@ -209,6 +217,7 @@ class RRT(Node):
 
         """
         self.occupancy_grid.populate(scan_msg)
+        self.grid_formed = True
         if DEBUG:
             self.occupancy_grid.publish_for_vis()
 
@@ -225,6 +234,20 @@ class RRT(Node):
         x_car = x * np.cos(yaw) + y * np.sin(yaw)
         y_car = -x * np.sin(yaw) + y * np.cos(yaw)
         return np.array([x_car, y_car])
+    
+
+    def transform_point_to_map_frame(self, point, car_pose):
+        # Transform the point to the map frame
+        x = point[0]
+        y = point[1]
+        # Rotate the point to the map frame
+        yaw = euler_from_quaternion([car_pose.orientation.x,
+                                       car_pose.orientation.y,
+                                       car_pose.orientation.z,
+                                       car_pose.orientation.w])[2]
+        x_map = x * np.cos(yaw) - y * np.sin(yaw) + car_pose.position.x
+        y_map = x * np.sin(yaw) + y * np.cos(yaw) + car_pose.position.y
+        return np.array([x_map, y_map])
 
 
     def visualize_goal(self, goal_point):
@@ -297,14 +320,24 @@ class RRT(Node):
         self.path_vis_pub_.publish(marker)
 
 
-    def publish_waypoint_to_track(self, waypoint):
+    def visualize_waypoint_to_track(self, waypoint):
         # Publish the waypoint to track
-        point = PointStamped()
-        point.header.frame_id = "ego_racecar/laser"
-        point.header.stamp = self.get_clock().now().to_msg()
-        point.point.x = waypoint[0]
-        point.point.y = waypoint[1]
-        self.waypoint_to_track_pub_.publish(point)
+        marker = Marker()
+        marker.header.frame_id = "ego_racecar/laser"
+        marker.header.stamp = self.get_clock().now().to_msg()
+        marker.type = Marker.SPHERE 
+        marker.action = Marker.ADD
+        marker.scale.x = 0.25
+        marker.scale.y = 0.25
+        marker.scale.z = 0.25
+        marker.color.a = 1.0
+        marker.color.r = 1.0
+        marker.color.g = 0.0
+        marker.color.b = 0.0
+        marker.pose.position.x = waypoint[0]
+        marker.pose.position.y = waypoint[1]
+        marker.pose.position.z = 0.0
+        self.waypoint_to_track_pub_.publish(marker)
 
 
     def rrt(self, goal):
@@ -351,7 +384,53 @@ class RRT(Node):
                 break
 
         return path
-    
+
+
+    def rrt_star(self, goal):
+        """
+        The main RRT* function
+        Args: 
+            goal (Pose): goal pose in car frame
+        Returns:
+            path ([]): list of points representing the path
+        """
+        # Initialize the tree with the start node
+        start_node = TreeNode()
+        start_node.x = 0.0
+        start_node.y = 0.0
+        start_node.cost = 0
+        start_node.is_root = True
+        self.tree = [start_node]
+
+        # Initialize the path with the start node
+        path = [start_node]
+
+        # RRT* loop
+        goal_with_min_cost = None
+        for _ in range(self.num_rrt_points):
+            # Sample a point in the free space
+            sampled_point = self.sample(goal)
+
+            # Calculate the cost of the new node
+            new_node = TreeNode()
+            new_node.x = sampled_point[0]
+            new_node.y = sampled_point[1]
+            new_node.cost, new_node.parent = self.calc_cost_new_node(self.tree, new_node)
+            if new_node.parent is None:
+                continue
+
+            self.tree.append(new_node)
+            self.rewire(self.tree, new_node)
+
+            # Check if the new node is close enough to the goal
+            if self.is_goal(new_node, goal[0], goal[1]):
+                if goal_with_min_cost is None or new_node.cost < goal_with_min_cost.cost:
+                    goal_with_min_cost = new_node
+
+        # Find the path from the closest node to the start node
+        path = self.find_path(self.tree, goal_with_min_cost)
+        return path
+
 
     def pure_pursuit(self, current_goal):
         """
@@ -385,6 +464,26 @@ class RRT(Node):
             self.get_logger().info(f'Speed: {drive_msg.drive.speed}')
         self.drive_pub_.publish(drive_msg)
 
+    
+    def find_waypoint_to_track(self, path):
+        """
+        Find the waypoint to track
+        Args: 
+            path ([]): list of points representing the path
+        Returns:
+            waypoint (tuple of (float, float)): the waypoint to track
+        """
+        # Find the waypoint to track
+        # waypoint = (path[0].x, path[0].y)
+        waypoint = None
+        print("Path length: ", len(path))
+        for node in path:
+            dist = LA.norm(np.array([node.x, node.y]))
+            if dist >= self.min_waypoint_tracking_distance:
+                waypoint = (node.x, node.y)
+                break
+        return waypoint
+
 
     def pose_callback(self, pose_msg):
         """
@@ -394,6 +493,9 @@ class RRT(Node):
         Args: 
             pose_msg (PoseStamped): incoming message from subscribed topic
         """
+        # if not self.grid_formed:
+        #     return
+
         # Get the current x, y position of the vehicle
         pose = pose_msg.pose.pose
         x = pose.position.x
@@ -409,15 +511,26 @@ class RRT(Node):
             if curr_dist > 0.8 * self.lookahead:
                 goal_point = self.waypoints[i, :2]
                 break
-        
+
         goal_point_car_frame = self.transform_point_to_car_frame(goal_point, pose)
         # Visualize the goal point
         if DEBUG:
             self.visualize_goal(goal_point_car_frame)
 
-        # Find the path from the current pose to the goal
-        path = self.rrt(goal_point_car_frame)
-        if path is None:
+        # # Find new plan if current waypoint is reached
+        # dist_to_waypoint = 0.0
+        # waypoint_to_track = None
+        # x_in_car_frame = 0.0
+        # if self.current_following_waypoint is not None:
+        #     waypoint_to_track = self.transform_point_to_car_frame(self.current_following_waypoint, pose)
+        #     dist_to_waypoint = LA.norm(waypoint_to_track)
+        #     x_in_car_frame = waypoint_to_track[0]
+        # if dist_to_waypoint < self.waypoint_close_enough or x_in_car_frame < 0:
+        # if True:
+            # Find the path from the current pose to the goal
+        # path = self.rrt(goal_point_car_frame)
+        path = self.rrt_star(goal_point_car_frame)
+        if path is None or len(path) < 2:
             return None
 
         # Visualize the path
@@ -426,14 +539,20 @@ class RRT(Node):
             self.visualize_tree()
 
         # Find the waypoint to track
-        waypoint_to_track = path[0]
-        if len(path) > 1:
-            waypoint_to_track = path[1]
+        # self.current_following_waypoint = self.find_waypoint_to_track(path)
+        waypoint_to_track = self.find_waypoint_to_track(path)
+        if DEBUG:
+            self.visualize_waypoint_to_track(waypoint_to_track)
+            # if self.current_following_waypoint is None:
+            #     print("No waypoint found")
+            #     print("Path length: ", len(path))
+            #     print("Path: ", path)
+            # self.current_following_waypoint = self.transform_point_to_map_frame(self.current_following_waypoint, pose)
 
-        # Publish the waypoint to track
-        # self.publish_waypoint_to_track((waypoint_to_track.x, waypoint_to_track.y))
-        # print(f'Waypoint to track: {waypoint_to_track.x}, {waypoint_to_track.y}')
-        self.pure_pursuit((waypoint_to_track.x, waypoint_to_track.y))
+            # waypoint_to_track = self.transform_point_to_car_frame(self.current_following_waypoint, pose)
+
+        # Pure pursuit in car frame
+        self.pure_pursuit(waypoint_to_track)
 
 
     def sample(self, goal):
@@ -449,13 +568,29 @@ class RRT(Node):
         # Goal biasing
         if np.random.uniform() < self.goal_bias:
             return (goal[0], goal[1])
+        
+        # Sample using anormal distribution with mean as the center between the goal and the car
+        # and standard deviation as the distance between the goal and the car / 2.
+        mean = np.array([goal[0], goal[1]]) / 2
+        std_dev = LA.norm(np.array([goal[0], goal[1]]) - np.array([0, 0])) / 2
+        x = np.random.normal(mean[0], std_dev)
+        y = np.random.normal(mean[1], std_dev/2)
 
-        x_bounds = self.grid_bounds_x
-        x = np.random.uniform(x_bounds[0], x_bounds[1])
+        # Clip the sampled point to the grid bounds
+        x = np.clip(x, self.grid_bounds_x[0]*0.8, self.grid_bounds_x[1]*0.8)
+        y = np.clip(y, self.grid_bounds_y[0]*0.8, self.grid_bounds_y[1]*0.8)
+
+        # x_bounds = self.grid_bounds_x
+        # x = np.random.uniform(x_bounds[0], x_bounds[1])
         # Sample y from -x to x, but also within the grid bounds for y
         # y_bounds = np.max([-x, self.grid_bounds_y[0]]), np.min([x, self.grid_bounds_y[1]])
-        y_bounds = self.grid_bounds_y
-        y = np.random.uniform(y_bounds[0], y_bounds[1])
+        # y_bounds = self.grid_bounds_y
+        # y = np.random.uniform(y_bounds[0], y_bounds[1])
+
+        # Check if the sampled point is in collision
+        # if self.occupancy_grid[x, y] == 1:
+        #     return self.sample(goal)
+
         return (x, y)
 
 
@@ -492,10 +627,10 @@ class RRT(Node):
         vec_to_sampled = vec_to_sampled / dist_to_sampled
         # if the distance to the sampled point is greater than the max distance
         # then we should only move max distance in the direction of the sampled point
-        if dist_to_sampled > self.max_distance:
+        if dist_to_sampled > self.max_steer_distance:
             new_node = TreeNode()
-            new_node.x = nearest_node.x + self.max_distance * vec_to_sampled[0]
-            new_node.y = nearest_node.y + self.max_distance * vec_to_sampled[1]
+            new_node.x = nearest_node.x + self.max_steer_distance * vec_to_sampled[0]
+            new_node.y = nearest_node.y + self.max_steer_distance * vec_to_sampled[1]
             new_node.parent = nearest_node
         else:
             new_node = TreeNode()
@@ -504,6 +639,7 @@ class RRT(Node):
             new_node.parent = nearest_node
 
         return new_node
+
 
     def check_collision(self, nearest_node, new_node):
         """
@@ -530,6 +666,7 @@ class RRT(Node):
 
         return False
 
+
     def is_goal(self, latest_added_node, goal_x, goal_y):
         """
         This method should return whether the latest added node is close enough
@@ -546,6 +683,7 @@ class RRT(Node):
         if dist_to_goal <= self.goal_close_enough:
             return True
         return False
+
 
     def find_path(self, tree, latest_added_node):
         """
@@ -568,7 +706,7 @@ class RRT(Node):
 
 
     # The following methods are needed for RRT* and not RRT
-    def cost(self, tree, node):
+    def calc_cost_new_node(self, tree, node):
         """
         This method should return the cost of a node
 
@@ -577,7 +715,25 @@ class RRT(Node):
         Returns:
             cost (float): the cost value of the node
         """
-        return 0
+        # cost is the distance between the node and the root node
+        # we check for all the nodes in the neighborhood of the node
+        # and return the one with the least line cost to the current node
+        if node.is_root:
+            return 0
+        neighborhood = self.near(tree, node)
+        min_cost = float('inf')
+        parent = None
+        for n in neighborhood:
+            cost = self.line_cost(n, node) + n.cost
+            if cost < min_cost:
+                # check if the line between n and node is in collision
+                if self.check_collision(n, node):
+                    continue
+                min_cost = cost
+                parent = n
+            
+        return min_cost, parent
+
 
     def line_cost(self, n1, n2):
         """
@@ -589,7 +745,10 @@ class RRT(Node):
         Returns:
             cost (float): the cost value of the line
         """
-        return 0
+        # cost is the distance between n1 and n2 plus the cost of n1
+        cost_between_nodes = LA.norm(np.array([n1.x, n1.y]) - np.array([n2.x, n2.y]))
+        return cost_between_nodes
+
 
     def near(self, tree, node):
         """
@@ -602,7 +761,32 @@ class RRT(Node):
             neighborhood ([]): neighborhood of nodes as a list of Nodes
         """
         neighborhood = []
+        for n in tree:
+            dist = LA.norm(np.array([n.x, n.y]) - np.array([node.x, node.y]))
+            if dist <= self.neighborhood_radius and dist > 1e-3:
+                neighborhood.append(n)
         return neighborhood
+
+
+    def rewire(self, tree, node):
+        """
+        This method should rewire the tree such that the cost of the nodes
+        are minimized
+
+        Args:
+            tree ([]): current tree as a list of Nodes
+            node (Node): current node we're rewiring the tree for
+        """
+        neighborhood = self.near(tree, node)
+        for n in neighborhood:
+            new_cost = self.line_cost(n, node) + node.cost
+            if new_cost < n.cost:
+                # check if the line between n and node is in collision
+                if self.check_collision(n, node):
+                    continue
+                n.parent = node
+                n.cost = new_cost
+
 
 def main(args=None):
     rclpy.init(args=args)
